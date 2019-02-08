34c9a08610027c9a520dfc39764559b3497e9dda
palacios25 <mpalacios4@ucol.mx>
2019-02-08 15:29:30 -0600

    08/02/19

--- Auto.js
+++ Auto.js
@@ -1,9 +1,37 @@
-class Auto {
-    constructor(marca, modelo, color, kilometraje){
-        this.marca = marca;
-        this.modelo = modelo;
-        this.color = color;
-        this.kilometraje = kilometraje;
-        this.estado = "Apagado";
+export default class Auto {
+    constructor(marca, modelo, color ="negro", kilometraje = 0){
+        this._marca = marca;
+        this._modelo = modelo;
+        this._color = color;
+        this._kilometraje = kilometraje;
+        this._estado = "Apagado";
     }
+    get color(){
+        return this._color;
+    }
+    set color(color){
+        this._color = color.toUpperCase();//Controla que este en mayusculas
+    }
+    mostrarEstado(){
+        console.log (`El auto ${this._marca} ${this._color} del año 
+        ${this._modelo} tiene un kilometraje de ${this._kilometraje}
+        kms y esta ${this._estado}`);
+    }
+    encender (){
+        this._estado = "Encendido";
+        this.mostrarEstado();
+    }
+    apagar (){
+        this._estado = "Apagar";
+        this.mostrarEstado();
+    }
+    avanzar (velocidad, tiempo){
+        if (this._estado === "Apagado"){
+        console.log("El auto no puede avanzar por que esta apagado");
+         } else{
+             let distancia = velocidad * tiempo;
+             this._kilometraje = this._kilometraje + distancia;
+             this.mostrarEstado();
+         }
+     }
 }
